8918044981b3fec16f87871a0002d3cdd1b0e2a3
ÇETİN <cetinnnarasss@gmail.com>
2023-01-06 06:02:18 +0300

    start

--- 02-tours/setup/src/App.js
+++ 02-tours/setup/src/App.js
@@ -4,7 +4,9 @@ import Tours from './Tours'
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
 const url = 'https://course-api.com/react-tours-project'
+
 function App() {
+  const [loading, setLoading] = useState(true);
   return <h2>Tours Project Setup</h2>
 }
 
