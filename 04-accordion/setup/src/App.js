93d0378818b20f1f1c4acd030ae5e62b7335425b
ÇETİN <cetinnnarasss@gmail.com>
2023-01-15 23:25:44 +0300

    useState

--- 04-accordion/setup/src/App.js
+++ 04-accordion/setup/src/App.js
@@ -2,7 +2,20 @@ import React, { useState } from 'react';
 import data from './data';
 import SingleQuestion from './Question';
 function App() {
-  return <h2>accordion project setup</h2>;
+  const [question, setQustions] = useState(data);
+  return <manin>
+    <div className="container">
+      <h3>question and answers about login</h3>
+        <section className="info">
+          {
+           question.map((question) => {
+             return <SingleQuestion key={question.id} {...question} />;
+            })
+          }
+    
+        </section>
+    </div>
+  </manin>;
 }
 
 export default App;
