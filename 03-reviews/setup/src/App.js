c49be3d0c1ba5813212ef93d5ff8ab3616eadfe5
ÇETİN <cetinnnarasss@gmail.com>
2023-01-15 23:09:03 +0300

    finish

--- 03-reviews/setup/src/App.js
+++ 03-reviews/setup/src/App.js
@@ -2,15 +2,15 @@ import React from 'react';
 import Review from './Review';
 function App() {
   return (
-  <main>
-    <section className="container">
-      <div className="title">
-       <h2>our reviews</h2>
-       <div className="underline"></div>
-      </div>
-      <Review/>
-    </section>
-  </main>
+    <main>
+      <section className='container'>
+        <div className='title'>
+          <h2>our reviews</h2>
+          <div className='underline'></div>
+        </div>
+        <Review />
+      </section>
+    </main>
   );
 }
 
