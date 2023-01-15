fbdfe0cdfd6b53c7216b20f628c14d033a83a522
ÇETİN <cetinnnarasss@gmail.com>
2023-01-15 23:51:10 +0300

    states

--- 05-menu/setup/src/App.js
+++ 05-menu/setup/src/App.js
@@ -4,6 +4,9 @@ import Categories from './Categories';
 import items from './data';
 
 function App() {
+  const [menuItems, setMenuItems] = useState(items);
+  const [Categories, setCategories] = useState([]);
+
   return <h2>menu project setup</h2>;
 }
 
