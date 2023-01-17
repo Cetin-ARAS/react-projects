19b67afaa6e4f066333f68fe2057a352f1b0e15d
ÇETİN <cetinnnarasss@gmail.com>
2023-01-18 02:10:29 +0300

    finish

--- 06-tabs/setup/src/App.js
+++ 06-tabs/setup/src/App.js
@@ -36,9 +36,30 @@ function App() {
      </div>
      <div className="jobs-center">
        {/*btn container */}
+       <div className="btn-container">
+         {jobs.map((item, index) => {
+          return (
+            <button key={item.id} onClick={() =>
+            setValue(index)}
+              className={`job-btn ${index === value && 'activate-btn'} `}>
+              {item.company}
+            </button>
+          )
+         })}
+       </div>
        {/*job info */}
       <article className='job-info'>
         <h3>{title}</h3>
+        <h4>{company}</h4>
+        <p className='job-date'>{dates}</p>
+        {duties.map((duty, index) => {
+          return (
+            <div key={index} className='job-desc'>
+              <FaAngleDoubleRight className='job-icon'></FaAngleDoubleRight>
+              <p>{duty}</p>
+            </div>
+          )
+          })}
       </article>
      </div>
    </section> 
