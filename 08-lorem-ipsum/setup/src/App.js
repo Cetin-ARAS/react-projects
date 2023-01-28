4564e6a53bc0cad232ca8694df426a14f2c67ad1
ÇETİN <cetinnnarasss@gmail.com>
2023-01-28 23:44:34 +0300

    app2

--- 08-lorem-ipsum/setup/src/App.js
+++ 08-lorem-ipsum/setup/src/App.js
@@ -11,13 +11,14 @@ function App() {
   return (
   <section className='section-center'>
     <h3>tired of boring lorem ipsum?</h3>
-    <form className="lorem-form" onSubmit={handleSubmit}></form>
+    <form className="lorem-form" onSubmit={handleSubmit}>
     <label htmlFor="amount">
       paragraphs:
     </label>
     <input type="number" name="amount" id="amount" value={count}
     onChange={(e)=>setCount(e.target.value)}/>
-    <butto type="submit" className="btn">generator</butto>
+    <butto type="submit" className="btn">generate</butto>
+    </form>
   </section>
     );
 }
