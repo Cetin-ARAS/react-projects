1c25a02cbc22af5c8adce315dd573e6814b0c278
ÇETİN <cetinnnarasss@gmail.com>
2023-02-03 22:44:54 +0300

    app2

--- 10-grocery-bud/setup/src/App.js
+++ 10-grocery-bud/setup/src/App.js
@@ -6,8 +6,26 @@ function App() {
   const [name,setName] = useState('');
   const [list,setList] = useState([]);
   const [isEditing,setİsEditing] = useState(false);
-  
-  return <h2>grocery bud setup</h2>
+  const [editID,setEditID] = useState(null);
+  const [alert,setAlert] = useState({show:false,msg:'',type: ''});
+ 
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.
+  }
+  return (
+    
+    <section className='section-center'>
+       <form className='grocery-form' onSubmit={handleSubmit}>
+        {alert.show && < Alert />}
+       </form>
+       <div className="grocery-container">
+        <List />
+        <button className='clear-btn'>
+          clear items
+        </button>
+       </div>
+    </section>
 }
 
 export default App
