a7324f60347b7c0fdff3ff46b8e4b21cd0f9fcb1
ÇETİN <cetinnnarasss@gmail.com>
2023-02-19 08:38:11 +0300

    dark mode with useeffect

--- 26-react-redux-tailwind/my-project/src/components/Navbar.jsx
+++ 26-react-redux-tailwind/my-project/src/components/Navbar.jsx
@@ -1,16 +1,34 @@
-import React from 'react'
-import {BsLightbulb, BsBasketFill} from 'react-icons/bs';
+import { useEffect, useState } from 'react';
+import {BsLightbulb, BsBasketFill, BsMoonStarsFill} from 'react-icons/bs';
  
 const Navbar = () => {
+    const {color, setColor} = useState(false)
+
+    const root = document.getElementById("root");
+useEffect(()=>{
+    if(color){
+        root.style.backgroundColor= "black";
+        root.style.color= "grey";
+    }else{
+        root.style.backgroundColor= "white";
+        root.style.color= "black";
+    }
+},[color])
+
+
   return (
-    <div className='flex items-center justify-between px-3'>
+    <div className='flex items-center justify-between px-3 h-28'>
       <div>LOGO</div>
       <div className='flex items-center space-x-4'>
-        <input type="text" placeholder='search' />
-        <BsLightbulb />
+        <input className='border p-3 outline-none rounded-lg' type="text" placeholder='search' />
+        <div onClick={() => setColor(!color)}>
+            { color ? <BsMoonStarsFill size={25} className="cursor-pointer" /> : <BsLightbulb size={25} className="cursor-pointer" />  }
+        
+        </div>
+        
         <div className='relative'>
-          <BsBasketFill/>
-          <span className='px-1 bg-red-600 text-white rounded-full text-sm'>3</span>
+          <BsBasketFill  size={25} className="cursor-pointer"/>
+          <span className='absolute -top-0 -right-3 px-2 bg-red-600 text-white rounded-full text-sm'>3</span>
         </div>
        </div>
     </div>
